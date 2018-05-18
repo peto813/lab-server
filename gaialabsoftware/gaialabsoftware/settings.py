d413667722e5fb2167de8a666c6bfc349283ef2b
Einstein Millan <peto813@users.noreply.github.com>
2018-05-18 13:03:01 -0400

    Update settings.py

--- gaialabsoftware/gaialabsoftware/settings.py
+++ gaialabsoftware/gaialabsoftware/settings.py
@@ -131,11 +131,13 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 #'django.core.context_processors.request',
             ],
-            'loaders': [
+            'loaders':[
+                'admin_tools.template_loaders.Loader',
+                ('django.template.loaders.cached.Loader', [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
-                'admin_tools.template_loaders.Loader'
-            ]
+                ]),
+            ],
         },
     },
 ]
